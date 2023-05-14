6e569c685533280d7e261f22c7f10dbd700a539e
Sahil Saxena <122679806+saxenasahil873@users.noreply.github.com>
2023-05-14 02:28:10 -0700

    Add files via upload

---
+++ BMI-Calculator/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Sahil Saxena on 28/02/2023.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
